6945f6adb62eb87af125544ed0166deb03378ee6
jacobsoley <jacobsoley777@gmail.com>
2024-10-18 18:07:50 +0200

    18/10/2024

---
+++ expai.js
@@ -0,0 +1,48 @@
+//Escriu-los en un paper i desprès executa el codi. Et donen el mateix?
+//let b = 18;
+// let c = 15;
+// let d = 22;
+// a = a + 10;
+// b = b + 5 - c; //18+5-15=8
+// c = c + 4 + b;
+// d = d + b + a;
+// a = a + 1;
+// b = b + c;
+// c = b + c;
+// d = b + b;
+
+// IF-ELSE
+//14.2.- Escriu un programa que donat un número llegit amb prompt, escrigui el missatge “el
+//número llegit és parell” si el número es parell i “el número llegit es senar” en cas contrari.
+// let numeroparellsenar = prompt("Escriu un número");
+//   if (numeroparellsenar%2==0) {
+//     console.log("El número és parell");
+//     document.write("El número és parell");
+//     document.getElementById("Resposta1").innerHTML = "El número llegit és parell"
+//
+//   } else {
+//     console.log("El número és senar");
+//     document.write("El número és senar");
+//     document.getElementById("Resposta1").innerHTML = "El número llegit és senar"
+
+
+//SWITCH
+//15.1.- Fent servir la instrucció switch escriu un programa que donat amb prompt un número
+//de mes, entre 1 i 12, escrigui per pantalla el nom del mes que correspon.
+// switch (expression) {
+//   case expression:
+//
+//     break;
+//   default:
+//
+
+//FOR
+for (let i = 1; i <= 10; i++) {
+  document.write("Estem fent bucles");
+}
+
+// let j = 1;
+//
+// while (true) {
+//
+// }
